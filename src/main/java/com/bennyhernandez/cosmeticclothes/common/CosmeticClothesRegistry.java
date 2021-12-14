84b0c5ed8d8ad5ad65b6a7362e2cc00981e9b676
BennyFaelz <bfaelz@gmail.com>
2021-12-13 17:02:59 -0800

    First

---
+++ src/main/java/com/bennyhernandez/cosmeticclothes/common/CosmeticClothesRegistry.java
@@ -0,0 +1,30 @@
+package com.bennyhernandez.cosmeticclothes.common;
+
+import com.bennyhernandez.cosmeticclothes.common.item.TestBodyItem;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.ObjectHolder;
+
+@Mod.EventBusSubscriber(modid = "cosmeticclothes", bus = Mod.EventBusSubscriber.Bus.MOD)
+public class CosmeticClothesRegistry {
+    @ObjectHolder("armouroverlays:testbody")
+    public static final Item TESTBODY;
+
+    static {
+        TESTBODY = null;
+    }
+
+    @SubscribeEvent
+    public static void  registerItems(RegistryEvent.Register<Item> evt) {
+        IForgeRegistry<Item> registry = evt.getRegistry();
+        register(new TestBodyItem(), "testbody", registry);
+    }
+
+    private static  void register(Item item, String name, IForgeRegistry<Item> registry) {
+        item.setRegistryName("armouroverlays", name);
+        registry.register(item);
+    }
+}
---
+++ src/main/java/com/bennyhernandez/cosmeticclothes/common/item/TestBodyItem.java
@@ -0,0 +1,13 @@
+package com.bennyhernandez.cosmeticclothes.common.item;
+
+import net.minecraft.item.Item;
+import top.theillusivec4.curios.api.type.capability.ICurio;
+
+public class TestBodyItem extends Item implements ICurio {
+    public TestBodyItem() {
+        super(new Item.Properties());
+    }
+
+
+
+}
